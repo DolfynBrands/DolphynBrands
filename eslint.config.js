f52f22f6f273e5f96a9de7d4e3be5225bc4abee7
Sai Rishi Ganagarapu <sairishi.gangarapu@gmail.com>
2025-07-14 23:29:01 +0530

    mailing feature with gitattributes

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,9 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
+  {
+    ignores: ["backend/**"],
+  },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
